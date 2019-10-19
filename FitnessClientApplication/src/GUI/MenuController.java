d19db9db9c9b6876dee3c288059d54840577537b
Victor <vielm18@student.sdu.dk>
2019-10-19 20:22:12 +0200

    Menu Package Fix

--- FitnessClientApplication/src/GUI/MenuController.java
+++ FitnessClientApplication/src/GUI/MenuController.java
@@ -2,7 +2,11 @@ package GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -11,12 +15,79 @@ import javafx.fxml.Initializable;
  */
 public class MenuController implements Initializable {
 
+    @FXML
+    private ImageView todayImageButton;
+    @FXML
+    private Label todayLabel;
+    @FXML
+    private ImageView todayTextboxImage;
+    @FXML
+    private ImageView statsImageButton;
+    @FXML
+    private ImageView programImageButton;
+    @FXML
+    private ImageView profileImageButton;
+    @FXML
+    private ImageView emptyImageButton1;
+    @FXML
+    private ImageView emptyImageButton2;
+    @FXML
+    private Label statsLabel;
+    @FXML
+    private Label træningsPrgLabel;
+    @FXML
+    private Label profileLabel;
+    @FXML
+    private Label emptyLabel1;
+    @FXML
+    private Label emptyLabel2;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void handleTodayImageAction(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleTodayLabelAction(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleStatsImageButtonAction(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleProgramImageButtonAction(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleProfileImageButtonAction(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleEmptyImageButton1Action(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleEmptyImageButton2Action(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleStatsLabelAction(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleTræningsPrgLabelAction(MouseEvent event) {
+    }
+
+    @FXML
+    private void handleProfileLabelAction(MouseEvent event) {
+    }
+
 }
