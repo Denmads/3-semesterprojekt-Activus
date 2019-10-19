194ad82f2251847600cb60e8555441c37e010ad3
Sejen17 <43138770+Sejen17@users.noreply.github.com>
2019-10-19 17:49:01 +0200

    ProfileUI
    I have made the profile side, we have to make some changes but the outline should be there.

---
+++ FitnessClientApplication/src/GUI/ProfileController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+
+/**
+ * FXML Controller class
+ *
+ * @author Sebas
+ */
+public class ProfileController implements Initializable {
+    @FXML
+    private ImageView CoverPic;
+    @FXML
+    private Circle ProfilePic;
+    @FXML
+    private TextArea nameTextField;
+    @FXML
+    private TextArea CuntryTextField;
+    @FXML
+    private TextArea SexTextField;
+    @FXML
+    private TextArea GymTextField;
+    @FXML
+    private TextArea CityTextField;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
