88f4801e404e07d943e3a9ccbf66642afb0a487a
Steffen pedersen <stped18@student.sdu.dk>
2019-10-21 21:22:37 +0200

    menu

--- ActivusFitnessApp/app/src/main/java/com/example/activussfitnessapp/MainActivity.java
+++ ActivusFitnessApp/app/src/main/java/com/example/activussfitnessapp/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.activussfitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Entity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String userName;
+    private String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginBtn = (Button) findViewById(R.id.packed);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText un = (EditText) findViewById(R.id.UsernameOnPage);
+                EditText pw = (EditText) findViewById(R.id.passwordOnPage);
+                userName = un.getText().toString();
+                password = pw.getText().toString();
+                if(true){
+                    Intent goToMenu = new Intent(getApplicationContext(),Menu.class);
+                    startActivity(goToMenu);
+                }
+            }
+        });
     }
 }
