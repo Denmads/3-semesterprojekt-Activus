4c8dfb5d44751c4063b1e797895d66344ce3c3b3
TempestRules <43045051+TempestRules@users.noreply.github.com>
2019-10-21 09:54:46 +0200

    Menu & Clean

--- ActivusDataLibrary/src/Enums/RequestType.java
+++ ActivusDataLibrary/src/Enums/RequestType.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Enums;
 
 import java.io.Serializable;
@@ -11,7 +6,7 @@ import java.io.Serializable;
  *
  * @author madsh
  */
-public enum RequestType implements Serializable{
+public enum RequestType implements Serializable {
     LOGIN,
     LOGOUT,
     CREATE_NEW_USER,
