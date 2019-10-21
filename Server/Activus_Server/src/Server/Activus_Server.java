bf429211f5d74b3e47ee397c024a75c0216c3d1e
TempestRules <43045051+TempestRules@users.noreply.github.com>
2019-10-21 14:20:11 +0200

    Server clean & Commenting

--- Server/Activus_Server/src/Server/Activus_Server.java
+++ Server/Activus_Server/src/Server/Activus_Server.java
@@ -1,11 +1,6 @@
+package Server;
 
 import Communication.TcpServer;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -20,5 +15,5 @@ public class Activus_Server {
         TcpServer a = new TcpServer();
         a.start();
     }
-    
+
 }
