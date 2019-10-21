08ebf046d806435b8b23404e6be8a523a751bbc4
Steffen pedersen <stped18@student.sdu.dk>
2019-10-22 00:09:40 +0200

    virker

--- ActivusFitnessApp/app/src/main/java/com/example/activussfitnessapp/Menu.java
+++ ActivusFitnessApp/app/src/main/java/com/example/activussfitnessapp/Menu.java
@@ -2,60 +2,73 @@ package com.example.activussfitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 public class Menu extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        final ImageButton notificationbtn = (ImageButton) findViewById(R.id.notificationbtn);
+        ImageButton workoutNtm = (ImageButton) findViewById(R.id.workoutbtn);
+        ImageButton trainingprogramBtn = (ImageButton) findViewById(R.id.Trainingprogrambtn);
+        ImageButton profileBtn = (ImageButton) findViewById(R.id.profilbtn);
+        ImageButton statsBtn = (ImageButton) findViewById(R.id.statsbtn);
+        ImageButton inboksBtn = (ImageButton) findViewById(R.id.inboksbtn);
+        ImageButton exicieseBtn = (ImageButton) findViewById(R.id.excisebtn);
+        ImageButton chatBtn = (ImageButton) findViewById(R.id.chatbtn);
+        ImageButton calenderBtn = (ImageButton) findViewById(R.id.calenderbtn);
         ImageButton buddylistBtn = (ImageButton) findViewById(R.id.buddylistbtn);
+
         buddylistBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+            
             }
         });
 
-        ImageButton calenderBtn = (ImageButton) findViewById(R.id.calenderbtn);
+
         calenderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
-        ImageButton chatBtn = (ImageButton) findViewById(R.id.chatbtn);
+
         chatBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
-        ImageButton exicieseBtn = (ImageButton) findViewById(R.id.excisebtn);
+
         exicieseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
-        ImageButton inboksBtn = (ImageButton) findViewById(R.id.inboksbtn);
+
         inboksBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
-        ImageButton profileBtn = (ImageButton) findViewById(R.id.profilbtn);
+
         profileBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
-        ImageButton statsBtn = (ImageButton) findViewById(R.id.statsbtn);
+
         statsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,14 +76,14 @@ public class Menu extends AppCompatActivity {
             }
         });
 
-        ImageButton trainingprogramBtn = (ImageButton) findViewById(R.id.Trainingprogrambtn);
+
         trainingprogramBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
-        ImageButton workoutNtm = (ImageButton) findViewById(R.id.workoutbtn);
+
         calenderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,7 +91,7 @@ public class Menu extends AppCompatActivity {
             }
         });
 
-        ImageButton notificationbtn = (ImageButton) findViewById(R.id.notificationbtn);
+
         notificationbtn.setVisibility(View.INVISIBLE);
         notificationbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,7 +104,9 @@ public class Menu extends AppCompatActivity {
 
     }
 
-
+public void notification(ImageButton i){
+        i.setVisibility(View.VISIBLE);
+}
 
 
 
