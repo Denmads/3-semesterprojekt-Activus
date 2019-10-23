d914fdceaa46d4948d98a9ebd114a8b57d59d08e
AntiElysium <patrick@pilsby.dk>
2019-10-23 13:33:53 +0200

    Added CreateNewUserAction

---
+++ Server/Activus_Server/src/persistence/actions/CreateNewUserAction.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistence.actions;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jooq.DSLContext;
+import persistence.IDatabaseAction;
+import persistence.Util.PasswordTool;
+import static persistence.database.generated.Tables.LOGIN;
+import static persistence.database.generated.Tables.PROFILE;
+
+/**
+ *
+ * @author Patrick
+ */
+public class CreateNewUserAction extends IDatabaseAction<Boolean> {
+
+    private boolean executed;
+    private boolean result;
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    public CreateNewUserAction(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    protected void execute(DSLContext database) throws SQLException {
+        try {
+            database.insertInto(PROFILE).columns(PROFILE.FIRST_NAME, PROFILE.LAST_NAME).values(firstName, lastName).execute();
+            byte[] salt = PasswordTool.generateSalt();
+            byte[] hashedPassword = PasswordTool.hashPassword(password, salt);
+            database.insertInto(LOGIN).columns(LOGIN.USERNAME, LOGIN.HASH_PASSWORD, LOGIN.PASSWORD_SALT).values(username, hashedPassword, salt).execute();
+            executed = true;
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            Logger.getLogger(CreateNewUserAction.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public Boolean getResult() {
+        return executed ? result : false;
+    }
+
+    @Override
+    public boolean hasResult() {
+        return executed;
+    }
+
+}
