b878b3345a14184325077f874d522fb364879b00
AntiElysium <patrick@pilsby.dk>
2019-10-23 14:31:17 +0200

    Implemented handleRequest in AuthenticationRequestHandler
    Co-Authored-By: oblivoin <oblivoin@users.noreply.github.com>

--- Server/Activus_Server/src/domain/AuthenticationRequestHandler.java
+++ Server/Activus_Server/src/domain/AuthenticationRequestHandler.java
@@ -5,10 +5,19 @@
  */
 package domain;
 
+import Enums.RequestArgumentName;
+import static Enums.RequestType.CREATE_NEW_USER;
+import static Enums.RequestType.LOGIN;
+import static Enums.RequestType.LOGOUT;
 import Enums.ResponseArgumentName;
+import Exceptions.ArgumentNotFoundException;
 import Models.Request;
 import Models.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import persistence.DatabaseFacade;
+import persistence.actions.GetProfileByLoginIdAction;
+import persistence.actions.VerifyLoginAction;
 
 /**
  *
@@ -23,7 +32,21 @@ public class AuthenticationRequestHandler extends IRequestHandler{
     @Override
     public Response handleRequest(Request request) {
         Response response = new Response();
-        response.addArgument(ResponseArgumentName.SUCCESS, "It worked!");
+        
+        if(request.getRequestType().equals(CREATE_NEW_USER)){
+            response.addArgument(ResponseArgumentName.SUCCESS, true);
+        } else if (request.getRequestType().equals(LOGIN)){
+            try {
+                VerifyLoginAction vla = new VerifyLoginAction(request.getArgument(RequestArgumentName.USERNAME),request.getArgument(RequestArgumentName.PASSWORD));
+                GetProfileByLoginIdAction gpblia = new GetProfileByLoginIdAction(vla.getResult().getUserId());
+                response.addArgument(ResponseArgumentName.CREDENTIALS,vla.getResult());
+                response.addArgument(ResponseArgumentName.PROFILE, gpblia.getResult());
+            } catch (ArgumentNotFoundException | ClassCastException ex) {
+                Logger.getLogger(AuthenticationRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else if (request.getRequestType().equals(LOGOUT)){
+            response.addArgument(ResponseArgumentName.SUCCESS, true);
+        }
         
         return response;
     }
