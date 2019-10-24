8e7c8c2d9347d9282a85512ac6b4a9a1faff9f2e
Steffen pedersen <stped18@student.sdu.dk>
2019-10-24 13:12:01 +0200

    t

--- Server/Activus_Server/src/Domain/TrainingSchemeRequestHandler.java
+++ Server/Activus_Server/src/Domain/TrainingSchemeRequestHandler.java
@@ -21,6 +21,27 @@ public class TrainingSchemeRequestHandler extends IRequestHandler{
 
     @Override
     public Response handleRequest(Request request) {
+        Response response = new Response();
+        switch (request.getRequestType()){
+            case CREATE_TRAINING_PROGRAM:{
+                
+            }
+            case ADD_EXERCISE:{
+                
+            }   
+            case RECEIVE_EXERCISE:{
+                
+            }
+            case REMOVE_EXERCISE:{
+                
+            }
+            case LOAD_ALL_EXERCISE:{
+                
+            }
+            
+        }
+        
+        
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
---
+++ Server/Activus_Server/src/persistence/actions/CreateTrainingProgramAction.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistence.actions;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import org.jooq.DSLContext;
+import persistence.IDatabaseAction;
+import persistence.database.generated.tables.TrainingProgram;
+
+/**
+ *
+ * @author steff
+ */
+public class CreateTrainingProgramAction extends  IDatabaseAction<TrainingProgram>{
+    
+    private ArrayList<TrainingProgram> resultList = null;
+    
+    
+
+    @Override;
+    protected void execute(DSLContext database) throws SQLException {
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public TrainingProgram getResult() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean hasResult() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
