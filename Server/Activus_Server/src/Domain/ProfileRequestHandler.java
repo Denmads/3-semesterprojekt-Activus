4b9d4a567ba9eb06f89c9a35200f2471b04ed148
TempestRules <43045051+TempestRules@users.noreply.github.com>
2019-10-24 14:16:09 +0200

    UpdateProfileAction
    Klassen/Metoden er ikke færdig. Der er problemet med constructorens argumenter.

--- Server/Activus_Server/src/Domain/ProfileRequestHandler.java
+++ Server/Activus_Server/src/Domain/ProfileRequestHandler.java
@@ -6,9 +6,11 @@ import Exceptions.ArgumentNotFoundException;
 import Models.Request;
 import Models.Response;
 import Persistence.Actions.UpdatePasswordAction;
+import Persistence.Actions.UpdateProfileAction;
 import Persistence.Actions.UpdateUsernameAction;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.jooq.TableField;
 import persistence.DatabaseFacade;
 import persistence.actions.RemoveExerciseByIDAction;
 import persistence.actions.AddExerciseToTrainingProgramAction;
@@ -47,7 +49,7 @@ public class ProfileRequestHandler extends IRequestHandler {
                     Logger.getLogger(ProfileRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 break;
-            case SET_STAT: {
+            case SET_STAT: 
                 try {
                     SetStatsAction ssa = new SetStatsAction(request.getArgument(RequestArgumentName.PROFILE_ID), request.getArgument(RequestArgumentName.EXERCISE_ID), request.getArgument(RequestArgumentName.PROGRAM_TIME_TAKEN), request.getArgument(RequestArgumentName.PROGRAM_REPS), request.getArgument(RequestArgumentName.PROGRAM_SETS), request.getArgument(RequestArgumentName.PROGRAM_WEIGHT), request.getArgument(RequestArgumentName.PROGRAM_DATE));
                     response.addArgument(ResponseArgumentName.SUCCESS, ssa.getResult());
@@ -55,7 +57,14 @@ public class ProfileRequestHandler extends IRequestHandler {
                     Logger.getLogger(ProfileRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 break;
-            }
+            case UPDATE_PROFILE:
+                try {
+                    //FIGURE OUT: The arguments for the UpdateProfileAction constructor.
+                UpdateProfileAction upa = new UpdateProfileAction(request.getArgument(RequestArgumentName.PROFILE_ID), request.getArgument(RequestArgumentName.), Object obj);
+                response.addArgument(ResponseArgumentName.SUCCESS, upa.getResult());
+                } catch (ArgumentNotFoundException | ClassCastException ex) {
+                    Logger.getLogger(ProfileRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+                }
         }
         //UPDATE RETURN STATEMENT LATER!!!
         return null;
--- Server/Activus_Server/src/Persistence/Actions/UpdateProfileAction.java
+++ Server/Activus_Server/src/Persistence/Actions/UpdateProfileAction.java
@@ -1,41 +1,57 @@
 package Persistence.Actions;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import org.jooq.DSLContext;
+import org.jooq.Result;
+import org.jooq.Record;
+import org.jooq.TableField;
 import persistence.IDatabaseAction;
+import static persistence.database.generated.Tables.PROFILE;
 
 /**
  * This class is meant to update the chosen profile information in the database.
- * 
+ * THIS MIGHT NOT BE DONE!!!
+ *
  * @author Victor
  */
 public class UpdateProfileAction extends IDatabaseAction<Boolean> {
 
-    private ArrayList<> info;
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    private boolean updated = false;
+    private boolean executed = false;
+
+    private int profileID;
+    private TableField TF;
+    private Object changeObj;
+
+    public UpdateProfileAction(int profileID, TableField TF, Object changeObj) {
+        this.profileID = profileID;
+        this.TF = TF;
+        this.changeObj = changeObj;
+    }
+
     @Override
     protected void execute(DSLContext database) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //Updating chosen profile information:
+        database.update(PROFILE).set(TF, changeObj).where(PROFILE.ID.eq(profileID));
+
+        //Verify that the data has been updated:
+        Result<Record> res = database.select().from(PROFILE).where(PROFILE.ID.eq(profileID)).fetch();
+        Record record = res.get(0);
+
+        updated = record.getValue(TF).equals(changeObj);
+
+        executed = true;
+
     }
 
     @Override
     public Boolean getResult() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (executed ? updated : false);
     }
 
     @Override
     public boolean hasResult() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return executed;
     }
-    
-    
+
 }
