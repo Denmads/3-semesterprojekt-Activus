b62d5f8756019da298d73118b3e4fdd8a2e8e93b
AntiElysium <patrick@pilsby.dk>
2019-10-24 13:52:21 +0200

    Exercise

---
+++ ActivusDataLibrary/src/Models/Exercise.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Patrick
+ */
+public class Exercise implements Serializable {
+    private String name;
+    private String description;
+    private long timeTaken;
+    private int reps;
+    private int sets;
+    private int weight;
+
+    public Exercise() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getTimeTaken() {
+        return timeTaken;
+    }
+
+    public void setTimeTaken(long timeTaken) {
+        this.timeTaken = timeTaken;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    
+}
---
+++ Server/Activus_Server/src/persistence/actions/GetExerciseByIDAction.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistence.actions;
+
+import Models.Exercise;
+import java.sql.SQLException;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Record6;
+import org.jooq.Result;
+import persistence.IDatabaseAction;
+import static persistence.database.generated.Tables.EXERCISE;
+
+/**
+ *
+ * @author Patrick
+ */
+public class GetExerciseByIDAction extends IDatabaseAction<Exercise>{
+
+    private Exercise result;
+    private boolean executed;
+    
+    private int id;
+
+    public GetExerciseByIDAction(int id) {
+        this.id = id;
+    }
+    
+    @Override
+    protected void execute(DSLContext database) throws SQLException {
+        Result<Record6<String, String, Long, Integer, Integer, Integer>> res = database.select(EXERCISE.NAME,EXERCISE.DESCRIPTION,EXERCISE.TIME_TAKEN,EXERCISE.REBS,EXERCISE.SETS,EXERCISE.WEIGHT).from(EXERCISE.where(EXERCISE.ID.eq(id))).fetch();
+        result = new Exercise();
+        
+        Record rec = res.get(0);
+        result.setDescription(rec.get(EXERCISE.DESCRIPTION));
+        result.setName(rec.get(EXERCISE.NAME));
+        result.setReps(rec.get(EXERCISE.REBS));
+        result.setSets(rec.get(EXERCISE.SETS));
+        result.setTimeTaken(rec.get(EXERCISE.TIME_TAKEN));
+        result.setWeight(rec.get(EXERCISE.WEIGHT));
+    }
+
+    @Override
+    public Exercise getResult() {
+        return executed ? result : null;
+    }
+
+    @Override
+    public boolean hasResult() {
+        return executed;
+    }
+    
+}
