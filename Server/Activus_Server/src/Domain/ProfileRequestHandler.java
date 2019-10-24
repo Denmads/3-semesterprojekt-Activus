1597037d6a2373952fcba1078472db5e14a11060
AntiElysium <patrick@pilsby.dk>
2019-10-24 10:56:49 +0200

    Add exercise to program

--- Server/Activus_Server/src/Domain/ProfileRequestHandler.java
+++ Server/Activus_Server/src/Domain/ProfileRequestHandler.java
@@ -6,6 +6,7 @@ import Models.Request;
 import Models.Response;
 import persistence.DatabaseFacade;
 import persistence.actions.RemoveExerciseByIDAction;
+import persistence.actions.AddExerciseToTrainingProgramAction;
 
 /**
  *
@@ -19,11 +20,15 @@ public class ProfileRequestHandler extends IRequestHandler {
 
     @Override
     public Response handleRequest(Request request) {
+                Response response = new Response();
         switch(request.getRequestType()){
             case REMOVE_EXERCISE: 
                 RemoveExerciseByIDAction rebia = new RemoveExerciseByIDAction(request.getArgument(RequestArgumentName.EXERCISE_ID));
-                Response response = new Response();
                 response.addArgument(ResponseArgumentName.SUCCESS, rebia.getResult());
+                break;
+            case ADD_EXERCISE:
+                AddExerciseToTrainingProgramAction aettpa = new AddExerciseToTrainingProgramAction(request.getArgument(RequestArgumentName.EXERCISE_ID),request.getArgument(RequestArgumentName.PROGRAM_ID));
+                response.addArgument(ResponseArgumentName.SUCCESS, aettpa.getResult());
                 break;
         }
     }
---
+++ Server/Activus_Server/src/persistence/actions/AddExerciseToTrainingProgramAction.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistence.actions;
+
+import java.sql.SQLException;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import persistence.IDatabaseAction;
+import static persistence.database.generated.Tables.TRAINING_PROGRAM_EXERCISE;
+
+/**
+ * Action that connects an exercise to a training program in the DB
+ * @author Patrick
+ */
+public class AddExerciseToTrainingProgramAction extends IDatabaseAction<Boolean> {
+
+    private boolean result;
+    private boolean executed;
+    
+    private int exerciseID;
+    private int trainingProgramID;
+
+    public AddExerciseToTrainingProgramAction(int exerciseID, int trainingProgramID) {
+        this.exerciseID = exerciseID;
+        this.trainingProgramID = trainingProgramID;
+    }
+    
+    @Override
+    protected void execute(DSLContext database) throws SQLException {
+        database.insertInto(TRAINING_PROGRAM_EXERCISE).columns(TRAINING_PROGRAM_EXERCISE.EXERCISEID, TRAINING_PROGRAM_EXERCISE.TRAINING_PROGRAMID).values(exerciseID, trainingProgramID).execute();
+        executed = true;
+        
+        Result<Record> res = database.select(TRAINING_PROGRAM_EXERCISE.TRAINING_PROGRAMID).from(TRAINING_PROGRAM_EXERCISE).where(TRAINING_PROGRAM_EXERCISE.EXERCISEID.eq(exerciseID));
+        for(Record rec : res){
+            if(rec.equals(trainingProgramID)){
+                result = true;
+                break;
+            }
+        }
+        
+    }
+
+    @Override
+    public Boolean getResult() {
+        return executed ? result : false;
+    }
+
+    @Override
+    public boolean hasResult() {
+        return executed;
+    }
+    
+}
