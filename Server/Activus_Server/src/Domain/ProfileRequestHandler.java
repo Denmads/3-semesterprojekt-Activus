6a272aba46b15670c2ad32a9d229249b02e2a53e
TempestRules <43045051+TempestRules@users.noreply.github.com>
2019-10-24 11:05:01 +0200

    Added UpdatePasswordAction

--- Server/Activus_Server/src/Domain/ProfileRequestHandler.java
+++ Server/Activus_Server/src/Domain/ProfileRequestHandler.java
@@ -2,8 +2,12 @@ package Domain;
 
 import Enums.RequestArgumentName;
 import Enums.ResponseArgumentName;
+import Exceptions.ArgumentNotFoundException;
 import Models.Request;
 import Models.Response;
+import Persistence.Actions.UpdatePasswordAction;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import persistence.DatabaseFacade;
 import persistence.actions.RemoveExerciseByIDAction;
 import persistence.actions.AddExerciseToTrainingProgramAction;
@@ -20,17 +24,37 @@ public class ProfileRequestHandler extends IRequestHandler {
 
     @Override
     public Response handleRequest(Request request) {
-                Response response = new Response();
-        switch(request.getRequestType()){
-            case REMOVE_EXERCISE: 
-                RemoveExerciseByIDAction rebia = new RemoveExerciseByIDAction(request.getArgument(RequestArgumentName.EXERCISE_ID));
-                response.addArgument(ResponseArgumentName.SUCCESS, rebia.getResult());
+        Response response = new Response();
+        switch (request.getRequestType()) {
+            case REMOVE_EXERCISE:
+                RemoveExerciseByIDAction rebia;
+                try {
+                    rebia = new RemoveExerciseByIDAction(request.getArgument(RequestArgumentName.EXERCISE_ID));
+                    response.addArgument(ResponseArgumentName.SUCCESS, rebia.getResult());
+                } catch (ArgumentNotFoundException | ClassCastException ex) {
+                    Logger.getLogger(ProfileRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 break;
             case ADD_EXERCISE:
-                AddExerciseToTrainingProgramAction aettpa = new AddExerciseToTrainingProgramAction(request.getArgument(RequestArgumentName.EXERCISE_ID),request.getArgument(RequestArgumentName.PROGRAM_ID));
-                response.addArgument(ResponseArgumentName.SUCCESS, aettpa.getResult());
+                AddExerciseToTrainingProgramAction aettpa;
+                try {
+                    aettpa = new AddExerciseToTrainingProgramAction(request.getArgument(RequestArgumentName.EXERCISE_ID), request.getArgument(RequestArgumentName.PROGRAM_ID));
+                    response.addArgument(ResponseArgumentName.SUCCESS, aettpa.getResult());
+                } catch (ArgumentNotFoundException | ClassCastException ex) {
+                    Logger.getLogger(ProfileRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 break;
+            case UPDATE_PASSWORD:
+                UpdatePasswordAction upa;
+                try {
+                    upa = new UpdatePasswordAction(request.getArgument(RequestArgumentName.USERNAME), request.getArgument(RequestArgumentName.PASSWORD));
+                    response.addArgument(ResponseArgumentName.SUCCESS, upa);
+                } catch (ArgumentNotFoundException | ClassCastException ex) {
+                    Logger.getLogger(ProfileRequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+                }
         }
+        //UPDATE RETURN STATEMENT LATER!!!
+        return null;
     }
 
 }
