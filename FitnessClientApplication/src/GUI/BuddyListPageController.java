80e8e2ba8c534a63d54a5ced6106bb06ab952744
Mads Jensen <madsj18@student.sdu.dk>
2019-10-25 07:27:56 +0200

    Updated budy list page
    More design
    
    Animation added to message field on the onfocus and outfocus events

--- FitnessClientApplication/src/GUI/BuddyListPageController.java
+++ FitnessClientApplication/src/GUI/BuddyListPageController.java
@@ -15,16 +15,24 @@ import Models.Request;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -34,21 +42,22 @@ import javafx.scene.layout.VBox;
 public class BuddyListPageController extends PageHandler implements Initializable {
 
     
-    @FXML
     private AnchorPane pane;
-    @FXML
     private ImageView Menubtn;
-    @FXML
-    private AnchorPane tilePane;
-    @FXML
     private VBox Vboks;
     private AuthenticationService authenticationService;
+    @FXML
+    private ListView<?> ListViewBuddies;
+    @FXML
+    private TextArea messageField;
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {      
+        
+        createAnimationForMessageField();
         
       //TODO new to get the curret Profile
 //        for (int buddyID : currentProfile.getBuddyIds()) {
@@ -58,8 +67,31 @@ public class BuddyListPageController extends PageHandler implements Initializabl
 //        }
         
     }    
+    
+    private void createAnimationForMessageField () {
+        Timeline messageFocusTimeline = new Timeline();
+        KeyValue kv = new KeyValue(messageField.prefHeightProperty(), 100d, Interpolator.LINEAR);
+        KeyFrame kf = new KeyFrame(Duration.millis(100), kv);
+        messageFocusTimeline.getKeyFrames().add(kf);
+        
+        Timeline messageLostFocusTimeline = new Timeline();
+        KeyValue kv1 = new KeyValue(messageField.prefHeightProperty(), 50d, Interpolator.LINEAR);
+        KeyFrame kf1 = new KeyFrame(Duration.millis(100), kv1);
+        messageLostFocusTimeline.getKeyFrames().add(kf1);
+        
+        messageField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue) {
+                    messageFocusTimeline.play();
+                }
+                else {
+                    messageLostFocusTimeline.play();
+                }
+            }
+        });
+    }
 
-    @FXML
     private void ActionOnMouseClick(MouseEvent event) {
         if(event.getSource()==Menubtn){
             changeFxml(pane, "FXML/Menu.fxml");
