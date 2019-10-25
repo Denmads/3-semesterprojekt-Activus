c7580d6c1afca8f8454618d5228e6107df7f8f4d
Per Terp <pertl18@gmail.com>
2019-10-25 09:19:13 +0200

    small stuff png on menu

--- FitnessClientApplication/src/GUI/FXMain.java
+++ FitnessClientApplication/src/GUI/FXMain.java
@@ -16,7 +16,7 @@ public class FXMain extends Application {
   
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXML/BuddyListPage.fxml"));  
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/Menu.fxml"));  
         Scene scene = new Scene(root);       
         stage.setScene(scene);
         stage.show();
