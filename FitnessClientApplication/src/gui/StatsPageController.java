53d4ec3f383900baccb1e55e8382e3bbaa9c3a72
Steffen <Stped18@student.sdu.dk>
2019-11-04 10:37:36 +0100

    statsSide choisboks

--- FitnessClientApplication/src/gui/StatsPageController.java
+++ FitnessClientApplication/src/gui/StatsPageController.java
@@ -5,10 +5,23 @@
  */
 package gui;
 
+import Communication.CommunicationFacade;
+import Enums.ServiceType;
+import Exceptions.ConfigFileNotFound;
+import Exceptions.ServiceNotFoundException;
 import Models.Exercise;
+import domain.DomainFacade;
+import domain.serviceInterfaces.IProfileService;
+import domain.serviceInterfaces.ITrainingSchemeService;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.ChoiceBox;
@@ -26,15 +39,40 @@ public class StatsPageController extends ContentPageController {
     @FXML
     private LineChart<Integer, Date> charid;
     @FXML
-    private ChoiceBox<Exercise> choseBoks;
+    private ChoiceBox<String> choseBoks;
+    private ObservableList<Exercise> exerciseList;
     
-
     /**
      * Initializes the controller class.
+     * @param url
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+//        choseBoks.getItems().add(new Exercise(1, "pusups",0 ).getName());
+//        choseBoks.getItems().add(new Exercise(2, "pullups",0 ).getName());
+        // creating choisboks with users exercises
+        try {
+            domainFacade = new DomainFacade(new CommunicationFacade());
+           // exerciseList = FXCollections.observableArrayList(domainFacade.<IProfileService>getService(ServiceType.TRAININGSCHEME).getCurrentStats(domainFacade.<IProfileService>getService(ServiceType.PROFILE).getCurrentProfile().getProfileId()));
+           exerciseList = FXCollections.observableArrayList(domainFacade.<ITrainingSchemeService>getService(ServiceType.TRAININGSCHEME).loadAllExercise());
+            if (!exerciseList.isEmpty()) {
+                exerciseList.forEach((e) -> {
+                    choseBoks.getItems().add(e.getName());
+                });
+                
+            }
+
+        } catch (ServiceNotFoundException ex) {
+            Logger.getLogger(StatsPageController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassCastException ex) {
+            Logger.getLogger(StatsPageController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ConfigFileNotFound ex) {
+            Logger.getLogger(StatsPageController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(StatsPageController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
     }
 
 }
