165b522eac889bd77d47f7803cfe650ac70fd155
steffen <steffen@vittech.dk>
2019-11-14 22:38:55 +0100

    stats server stop

--- FitnessClientApplication/src/gui/StatsPageController.java
+++ FitnessClientApplication/src/gui/StatsPageController.java
@@ -13,6 +13,7 @@ import domain.serviceInterfaces.IProfileService;
 import java.net.URL;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,8 +21,10 @@ import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import models.Profile;
 import models.Stats;
 
 /**
@@ -34,7 +37,7 @@ public class StatsPageController extends ContentPageController {
     @FXML
     private Label exerciseName;
     @FXML
-    private LineChart<SetInfo, Date> charid;
+    private LineChart<?, ?> charid;
     @FXML
     private ChoiceBox<String> choseBoks;
    
@@ -46,42 +49,82 @@ public class StatsPageController extends ContentPageController {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        try {
+            Stats s = new Stats(domainFacade.<IProfileService>getService(ServiceType.PROFILE).getCurrentProfile());
+            Exercise e = new Exercise(0, "bænk", 0, "bryst");
+            for(int i = 0; i<3;i++){
+                SetInfo setinfo = new SetInfo((i+1), (i+10));
+                e.addSetInfo(setinfo);
+                
+            }
+            s.addExercises(e.getType(), new Date(), e);
+            XYChart.Series reps= new XYChart.Series<>();
+            XYChart.Series weight= new XYChart.Series<>();
+            
+            for (Map.Entry<String, HashMap> en : s.getStatsMap().entrySet()) {
+                Object key = en.getKey();
+                HashMap<Date,Exercise> value = en.getValue();
+                for (Map.Entry<Date, Exercise> entry : value.entrySet()) {
+                    Date key1 = entry.getKey();
+                    Exercise value1 = entry.getValue();
+                    for(SetInfo setInfo: value1.getSetInfo()){
+                        reps.getData().add(new XYChart.Data<>(key1.toString(),setInfo.getReps()));
+                        weight.getData().add(new XYChart.Data<>(key1.toString(),setInfo.getWeight()));
+                        
+                    }
+                    
+                    
+                }
+                
+            }
+            charid.getData().addAll(reps);
+            charid.getData().addAll(weight);
+            
+        } catch (ServiceNotFoundException ex) {
+            Logger.getLogger(StatsPageController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassCastException ex) {
+            Logger.getLogger(StatsPageController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void onContentInitialize() {       
          try {
-           Stats stats =(Stats) domainFacade.<IProfileService>getService(ServiceType.PROFILE).getCurrentStats(domainFacade.<IProfileService>getService(ServiceType.PROFILE).getCurrentProfile().getProfileId());
-        
-           stats.getStatsMap().entrySet().forEach((entry) -> {
+            Stats s = new Stats(domainFacade.<IProfileService>getService(ServiceType.PROFILE).getCurrentProfile());
+            Exercise e = new Exercise(0, "bænk", 0, "bryst");
+            for(int i = 0; i<3;i++){
+                SetInfo setinfo = new SetInfo((i+1), (i+10));
+                e.addSetInfo(setinfo);               
+            }
+            s.addExercises(e.getType(), new Date(), e);
+           //Stats stats =(Stats) domainFacade.<IProfileService>getService(ServiceType.PROFILE).getCurrentStats(domainFacade.<IProfileService>getService(ServiceType.PROFILE).getCurrentProfile().getProfileId());
+            Stats stats = s;
+            if(stats!=null){
+               System.out.println("Stats her "+stats);
+               stats.getStatsMap().entrySet().forEach((entry) -> {
                String key = entry.getKey();
                HashMap value = entry.getValue();
                choseBoks.getItems().add(key);
              });
-
             choseBoks.getSelectionModel().selectedIndexProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
-                exerciseName.setText(choseBoks.getItems().toString());
-                charid.setTitle(choseBoks.getItems().toString());
-                stats.getStatsMap().entrySet().forEach((entry) -> {
-                    Object key = entry.getKey();
-                    HashMap<Date,Exercise> value = entry.getValue();
-                    XYChart.Series series = new XYChart.Series();
-                   if (choseBoks.getItems()==key) {
-                       value.entrySet().forEach((entry1) -> {
-                           Date key1 = entry1.getKey();
-                           Exercise value1 = entry1.getValue();
-                           series.getData().add(new XYChart.Data(value1.getSetInfo(), key1));
-                       });
-                       charid.getData().addAll(series);
-                   }
-               });
+            exerciseName.setText(choseBoks.getItems().toString());
+            charid.setTitle(choseBoks.getItems().toString());             
            });
-
+                
+            }else{
+               showAlert("No Stats!", "There are not stats to your profile");
+               
+            }
         } catch (ServiceNotFoundException | ClassCastException ex) {
             Logger.getLogger(StatsPageController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
-        
-        
-        
     }
     
-
+        private void showAlert (String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
